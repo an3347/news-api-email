a75b331fe76e86db4fe75a8e944c8296b623f801
an3347 <akshat.nagpal6@gmail.com>
2024-06-06 01:18:55 -0400

    add links, specify language, limit news, add subject, control category

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
 from send_email import send_email
+from datetime import datetime
+
+today_date = datetime.today().strftime("%B %d of %Y")
+topic = "tesla"
 
 api_key = "87e7e1831b1e443c9f07453ace6e5cca"
-url = "https://newsapi.org/v2/everything?q=tesla" \
+url = "https://newsapi.org/v2/everything?" \
+      f"q={topic}" \
       "&sortBy=publishedAt&apiKey=" \
-      "87e7e1831b1e443c9f07453ace6e5cca"
+      "87e7e1831b1e443c9f07453ace6e5cca" \
+      "&language=hi"
 
 # Make request
 request = requests.get(url)
@@ -12,14 +18,16 @@ request = requests.get(url)
 # Get a dictionary with data
 content = request.json()
 
-body = """\
-Subject: News of the day
+body = f"""\
+Subject: Hindi News on {today_date}
 
 """
 # Access the article titles and description
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None and article["description"] is not None:
-        body = body + article["title"] + "\n" + article["description"] + 2 * "\n"
+        body = (body + article["title"] + "\n"
+                + article["description"] + "\n"
+                + article["url"] + 2 * "\n")
 
 body = body.encode("utf-8")
 send_email(message=body)
