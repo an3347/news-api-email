9d5ddd0779ddbbd8c6e3410dd04b459aaaa90ed3
an3347 <akshat.nagpal6@gmail.com>
2024-06-06 12:41:18 -0400

    just for fun added 2 emails

--- main.py
+++ main.py
@@ -6,28 +6,34 @@ today_date = datetime.today().strftime("%B %d of %Y")
 topic = "tesla"
 
 api_key = "87e7e1831b1e443c9f07453ace6e5cca"
-url = "https://newsapi.org/v2/everything?" \
-      f"q={topic}" \
-      "&sortBy=publishedAt&apiKey=" \
-      "87e7e1831b1e443c9f07453ace6e5cca" \
-      "&language=hi"
 
-# Make request
-request = requests.get(url)
+urls = ["https://newsapi.org/v2/everything?"
+        f"q={topic}"
+        "&sortBy=publishedAt&apiKey="
+        "87e7e1831b1e443c9f07453ace6e5cca"
+        "&language=hi",
+        "https://newsapi.org/v2/top-headlines"
+        "?country=in"
+        f"&apiKey={api_key}"]
 
-# Get a dictionary with data
-content = request.json()
+for url in urls:
 
-body = f"""\
-Subject: Hindi News on {today_date}
+    # Make request
+    request = requests.get(url)
 
-"""
-# Access the article titles and description
-for article in content["articles"][:20]:
-    if article["title"] is not None and article["description"] is not None:
-        body = (body + article["title"] + "\n"
-                + article["description"] + "\n"
-                + article["url"] + 2 * "\n")
+    # Get a dictionary with data
+    content = request.json()
 
-body = body.encode("utf-8")
-send_email(message=body)
+    body = f"""\
+    Subject: News on {today_date}
+    
+    """
+    # Access the article titles and description
+    for article in content["articles"][:20]:
+        if article["title"] is not None and article["description"] is not None:
+            body = (body + article["title"] + "\n"
+                    + article["description"] + "\n"
+                    + article["url"] + 2 * "\n")
+
+    body = body.encode("utf-8")
+    send_email(message=body)
