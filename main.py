4461b6c65e676866aefb9b62b4a919c480b7dd77
an3347 <akshat.nagpal6@gmail.com>
2024-06-06 00:28:10 -0400

    Access the news title and description

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ url = "https://newsapi.org/v2/everything?q=tesla" \
       "&sortBy=publishedAt&apiKey=" \
       "87e7e1831b1e443c9f07453ace6e5cca"
 
+# Make request
 request = requests.get(url)
-content = request.text
-print(content)
+
+# Get a dictionary with data
+content = request.json()
+
+# Access the article titles and description
+for article in content["articles"]:
+    print(article["title"])
+    print(article["description"])
